6e6f487ff8685d8e3db0e4cb7c1da7a4752f0c61
Amarthya Ravi <41585469+Amarthya03@users.noreply.github.com>
2019-09-04 15:44:36 +0530

    Add files via upload
    Creating a tuple and performing various operations.

---
+++ Week_2_Assignments/lab2d_assignment2.py
@@ -0,0 +1,4 @@
+t = (23,30,11)                  #Creating a tuple
+print(t[1])                     #Printing one element of that tuple
+print(t)
+print(list(t))                  #Converting tuple to a list and printing
